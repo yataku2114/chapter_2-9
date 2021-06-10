6affdead347474e1f18a71e0cec36eff19b6f447
yataku2114 <yataku2114@gmail.com>
2021-06-10 15:25:47 +0000

    [Start] 2-7

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -4,17 +4,24 @@ class TodolistsController < ApplicationController
   end
   
   def create
-    list = List.new
-    
+    list = List.new(list_params)
     list.save
     
-    redirect_to "/top"
+    redirect_to todolist_path(list.id)
+  end
+  
+  def index
+    @lists = List.all
+  end
+  
+  def show
+    @list = List.find(params[:id])
   end
   
   private
   
   def list_params
-    params.require(:list).permit(:title,:body)
+    params.require(:list).permit(:title, :body)
   end
 
 end
