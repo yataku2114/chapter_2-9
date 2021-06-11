3e2b03bf95f49323b4a4495ea0d5f0097c221c39
yataku2114 <yataku2114@gmail.com>
2021-06-11 13:44:01 +0000

    [Start] 2-10

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a2eeb8baa1ef33015bce5166be573219459f79223e06572f26ac28c5d2f62849e342831899a8052757614fa14b5bd67974050775e219e84b04e1fd88ec94faed'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "todolists/new"
-  get "top" => "homes#top"
+  get "top" => "home#top"
   post "todolists" => "todolists#create"
   get "todolists" => "todolists#index"
-  
   get "todolists/:id" => "todolists#show", as: "todolist"
+  get "todolists/:id/edit" => "todolists#edit", as: "edit_todolist"
+  patch "todolists/:id/" => "todolists#update", as: "update_todolist"
 end
